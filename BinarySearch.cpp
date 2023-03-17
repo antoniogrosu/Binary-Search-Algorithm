3642c19ad503a9a74062a021288b5e0f874e01a4
Antonio Grosu <antonio.webdesign19@gmail.com>
2023-03-17 08:51:56 +0200

    code

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -3,20 +3,20 @@ using namespace std;
 
 int binarySearch(int v[10001],int target)
 {
-	bool found = 0;
+	bool found = 0; //helps in determining if a there's target's value in the array
 	int r = 0;
-	for (int i = 0;i <= 10000;i++)
+	for (int i = 0;i <= 10000;i++) //count the number of elements
 	{
 		if ((v[i] == 0) && (v[i] < v[i + 1])) r++;;
 		if (v[i] && (v[i] > v[i - 1])) r++;
 	}
 	int left = 0;
 	int right = r;
-	while (left <= right)
-	{
+	while (left <= right) //this loop is changing either left's or right's value based on the target value 
+	{					//in order to find target's value in the array
 		int mid = (right + left) / 2;
 		if (v[mid] == target) {
-			return mid;
+			return mid-1;
 			found = 1;
 			break;
 		}
@@ -25,6 +25,18 @@ int binarySearch(int v[10001],int target)
 		if (v[mid] > target)
 			right = mid - 1;
 	}
-	if (found == 0) return -1;
+	if (!found) return -1;
+}
+int v[10001];
+int main()
+{
+	int n,target;
+	cin >> n >> target;
+	for (int i = 1;i <= n;i++) // fill in the array
+	{
+		cin >> v[i];
+	}
+	cout << binarySearch(v, target);
+	
 }
 
